94d107e5778336d46cf0e7f34a538bff083e76f1
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-05-28 22:40:51 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ import "./App.css";
 // units determine hoe frequestly the subject will appear on the table
 // lunch time
 
+//data needed
+//subjects - name unit
+//starttimee
+//end time
+//perPeriod
+
 function App() {
+  //Constants
   const subjects = [
     {
       name: "csc211",
@@ -50,14 +57,61 @@ function App() {
       unit: 1,
     },
   ];
-  const hoursPerweek = 30;
-  const hourPerday = 6;
-  const minPerPeriod = 30;
+  const startTime = 8.0;
+  const endTime = 14;
+  const perPeriod = 0.3;
+  const noOfPeriod = 8;
 
-  const tt = subjects.map((subject) => {});
+  let table = {};
+  let Header = [];
+
+  const days = ["Moday", "Tuesday", "wednesday", "thursday", "friday"];
+  const generateTT = () => {
+    for (let i = startTime; i <= endTime; i++) {
+      Header.push(i + perPeriod);
+    }
+    const firstPriority = subjects.filter((sub) => sub.unit === 3);
+    console.log(firstPriority);
+
+    const secondPriority = subjects.filter((sub) => sub.unit === 2);
+    console.log(secondPriority);
+
+    const thirdPriority = subjects.filter((sub) => sub.unit === 1);
+    console.log(thirdPriority);
+
+    let subjects1 = [];
+    let subjects2 = [];
+    let subjects3 = [];
+    let subjects4 = [];
+    let subjects5 = [];
+
+    let finalSubject = [subjects1, subjects2, subjects3, subjects4, subjects5];
+
+    for (let i = 0; i < firstPriority.length; i++) {
+      finalSubject.forEach((fsub) => fsub.push(firstPriority[i++]));
+    }
+    for (let i = 0; i < secondPriority.length; i++) {
+      finalSubject.forEach((fsub) => fsub.push(secondPriority[i++]));
+    }
+    for (let i = 0; i < thirdPriority.length; i++) {
+      finalSubject.forEach((fsub) => fsub.push(thirdPriority[i++]));
+    }
+
+    console.log(finalSubject);
+    for (let j = 0; j < days.length; j++) {
+      for (let i = 0; i < subjects.length; i++) {
+        table[days[j]] = [finalSubject[j]];
+      }
+    }
+    console.table(table);
+    // console.log(table);
+  };
+  generateTT();
+
   return (
     <div className="App">
-      {subjects.map((subject) => console.table(subject))}
+      hwll
+      {/* {subjects.map((subject) => console.table(subject))} */}
     </div>
   );
 }
